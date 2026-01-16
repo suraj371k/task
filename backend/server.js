d8cb39cf1046deedef5349341739d622a1c7f1b2
suraj371k <surajkushwaha371@gmail.com>
2026-01-16 20:01:13 +0530

    first-commit

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+const port = 4000;
+
+//todo
+function add(...num) {
+  return num.reduce((acc, n) => acc + n, 0)
+}
+
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
